7438a41b55e32b3d291d3341f2191f8f1192453d
Jorge Romero <emilimacbook@MacBook-Air-de-Jorge.local>
2023-09-25 12:20:51 -0500

    Creación del proyecto angular, primer avance nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+
+const routes: Routes = [
+  {
+    path:'home', 
+    component: HomeComponent
+  },
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
